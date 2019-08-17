c916cfef5cf922619c14c3bf2628bf4dbaac307e
krakow10 <krakow10@live.ca>
2019-08-17 09:42:26 -0700

    Implement uint32_t buffer

--- ADM_vidBlendFrames.cpp
+++ ADM_vidBlendFrames.cpp
@@ -24,9 +24,9 @@ class AVDM_BlendFrames : public  ADM_coreVideoFilterCached
 {
 protected:
                 blend         param;
-                ADMImage     *buffer;
-                void         AccumulateFrame(ADMImage *buffer,ADMImage *frame);
-                void         WriteFrameAndClearBuffer(ADMImage *buffer,ADMImage *frame,uint32_t N);
+                uint32_t     ***buffer;
+                //void         AccumulateFrame(ADMImage *buffer,ADMImage *frame);
+                //void         WriteFrameAndClearBuffer(ADMImage *buffer,ADMImage *frame,uint32_t N);
 public:
                              AVDM_BlendFrames(ADM_coreVideoFilter *previous,CONFcouple *conf);
                              ~AVDM_BlendFrames();
@@ -86,7 +86,7 @@ const char *AVDM_BlendFrames::getConfiguration(void)
  * @param in
  * @param couples
  */
-AVDM_BlendFrames::AVDM_BlendFrames(ADM_coreVideoFilter *in,CONFcouple *setup) : ADM_coreVideoFilterCached(3,in,setup)//Q What does the 3 mean here?
+AVDM_BlendFrames::AVDM_BlendFrames(ADM_coreVideoFilter *in,CONFcouple *setup) : ADM_coreVideoFilterCached(1,in,setup)//Q What does the 3 mean here?
 {
     if(!setup || !ADM_paramLoad(setup,blend_param,&param))
     {
@@ -123,7 +123,7 @@ AVDM_BlendFrames::~AVDM_BlendFrames(void)
 {
     if(buffer)      
     {
-      delete buffer;
+      delete buffer;//Q How do I delete this monster?
       buffer=NULL;
     }
 }
@@ -135,6 +135,9 @@ AVDM_BlendFrames::~AVDM_BlendFrames(void)
  * @param offset
  * @return 
  */
+
+//Not gonna deal with passing triple pointers
+/*
 void AVDM_BlendFrames::AccumulateFrame(ADMImage *buffer,ADMImage *frame)
 {
     uint8_t *bplanes[3]*;//Q uint32_t type image for accumulation?
@@ -197,19 +200,13 @@ void AVDM_BlendFrames::WriteFrameAndClearBuffer(ADMImage *buffer,ADMImage *frame
         }        
     }
 }
-
-/**
- * \fn getNextFrame
- * @param fn
- * @param image
- * @return 
- */
 bool AVDM_BlendFrames::getNextFrame(uint32_t *fn,ADMImage *image)
 {
   ADMImage *frame=vidCache->getImage(fn);
-  //Create new buffer
-  if(buffer==NULL)
+  //Create new 32 bit accumulation buffer
+  if(buffer==NULL){
     buffer=new ADMImageDefault(frame->GetWidth(PLANAR_Y),frame->GetHeight(PLANAR_Y));
+  }
   AccumulateFrame(buffer,frame);
   accumulated++;
   if(accumulated==param.N){
@@ -217,11 +214,84 @@ bool AVDM_BlendFrames::getNextFrame(uint32_t *fn,ADMImage *image)
     //Divide buffer by N and write to 'image'
     //Reset buffer to 0
     WriteFrameAndClearBuffer(buffer,frame,param.N);
-    if(image->Pts==ADM_NO_PTS)
-        return true;
-    frame->Pts/=param.N;//Looking at changeFps filter, seems like there is no need to specify an invariant point
+    if(frame->Pts!=ADM_NO_PTS)
+        frame->Pts/=param.N;//Looking at changeFps filter, seems like there is no need to specify an invariant point
+    image->duplicate(frame);
     return true;
   }
   return false;
+}
+*/
+
+bool AVDM_BlendFrames::getNextFrame(uint32_t *fn,ADMImage *image)
+{
+	//I have a feeling that this is not the usual way to grab the input frame
+	ADMImage *frame=vidCache->getImage(fn);
+	//Create buffer
+	if(buffer==NULL){
+		buffer = new uint32_t**[3];
+		for(int i=0;i<3;i++)
+		{
+			int w=(int)frame->GetWidth((ADM_PLANE)i);
+			int h=(int)frame->GetHeight((ADM_PLANE)i);
+			buffer[i] = new uint32_t*[h];
+			for(int y=0;y<h;y++)
+			{
+				buffer[i][y] = new uint32_t[w];
+				for(int x=0;x<w;x++)
+				{
+					buffer[i][y][x]=0;
+				}
+			}
+		}
+	}
+	//Accumulate frame into buffer
+	uint8_t *fplanes[3]*;
+	int fpitches[3];
+	frame->GetReadPlanes(fplanes);
+	frame->GetPitches(fpitches);
+	for(int i=0;i<3;i++)
+	{
+		int w=(int)frame->GetWidth((ADM_PLANE)i);
+		int h=(int)frame->GetHeight((ADM_PLANE)i);
+		uint8_t *f=fplanes[i];
+		for(int y=0;y<h;y++)
+		{
+			for(int x=0;x<w;x++)
+			{
+				buffer[i][y][x]+=(uint32_t)f[x];
+			}
+			f+=fpitches[i];
+		}        
+	}
+	accumulated++;
+	//Output a frame
+	if(accumulated==param.N){
+		accumulated=0;
+		//Divide buffer by N and write to 'image'
+		image=new ADMImageDefault(frame->GetWidth(PLANAR_Y),frame->GetHeight(PLANAR_Y));
+		image->copyInfo(frame);//Who knows what crazy info the frame has
+		if(frame->Pts!=ADM_NO_PTS)
+			image->Pts=frame->Pts/param.N;
+		uint8_t *iplanes[3]*;
+		image->GetWritePlanes(iplanes);
+		for(int i=0;i<3;i++)
+		{
+			int w=(int)frame->GetWidth((ADM_PLANE)i);
+			int h=(int)frame->GetHeight((ADM_PLANE)i);
+			uint8_t *ip=iplanes[i];
+			for(int y=0;y<h;y++)
+			{
+				for(int x=0;x<w;x++)
+				{
+					ip[x]=(uint8_t)buffer[i][y][x]/param.N;//Not sure if this will divide before casting
+					buffer[i][y][x]=0;//Reset buffer to 0
+				}
+				ip+=fpitches[i];
+			}        
+		}
+		return true;
+	}
+	return false;
 }
 //EOF
